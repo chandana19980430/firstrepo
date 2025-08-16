4b7c8990bed6198f1a925744edb9c44dd4fa3187
chandana19980430 <nalladimmuchandana@gmail.com>
2025-08-16 12:54:56 +0530

    Update app.java

--- app.java
+++ app.java
@@ -5,15 +5,6 @@
         System.out.format("Standard Deviation = %.6f", SD);
     }
 
-    public static double calculateSD(double numArray[])
-    {
-        double sum = 0.0, standardDeviation = 0.0;
-        int length = numArray.length;
-
-        for(double num : numArray) {
-            sum += num;
-        }
-
         double mean = sum/length;
 
         for(double num: numArray) {
